65d5e71d4c74309ff3126f1e69123c3192cc2fe5
Yasser Jammeli <yasser.omar.jammeli@gmail.com>
2022-06-21 23:59:49 +0100

    add universities repo

---
+++ lib/modules/universities/domain/repository/universities_repository.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_clean_arch_template/core/common_model/api_result_model.dart';
+import 'package:flutter_clean_arch_template/modules/universities/domain/entities/university_entity.dart';
+
+abstract class UniversitiesRepository {
+  Future<ApiResultModel<List<UniversityEntity>>> getCountryUniversities(String country);
+  Future<ApiResultModel<List<UniversityEntity>>> getCountryUniversitiesByName(String country, String name);
+  Future<ApiResultModel<List<UniversityEntity>>> getUniversitiesByName(String name);
+}
