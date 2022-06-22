53e140919cf82d15fbc8265981ef53a3b4e29a4c
Yasser Jammeli <yasser.omar.jammeli@gmail.com>
2022-06-22 17:55:59 +0100

    reorder imports

--- test/core/utils/connectivity/network_info_test.dart
+++ test/core/utils/connectivity/network_info_test.dart
@@ -1,8 +1,8 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_clean_arch_template/core/utils/connectivity/network_info.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
-import 'package:flutter_test/flutter_test.dart';
 
 import 'network_info_test.mocks.dart';
 
